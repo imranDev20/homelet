be31d83ede2e0493a04a376a441660fc90ab2ad6
imrandev20 <imrandev20@gmail.com>
2022-01-12 20:48:08 +0600

    Changed Header.js to header.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,7 +4,7 @@ import { GlobalStyles } from "twin.macro"
 import { Main } from "../styles/Layout.styles"
 
 import Footer from "./Footer"
-import Header from "./Header"
+import Header from "./header"
 
 const Layout = ({ children }) => {
   return (
